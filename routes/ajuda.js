8118db678b61b68e4f5cca66a30cc725acefafe1
Sophia Moura <124884847+sophimoura@users.noreply.github.com>
2024-04-23 17:06:32 -0300

    Delete routes directory

--- routes/ajuda.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('ajuda', { title: 'Ajuda' });
-});
-
-module.exports = router;
